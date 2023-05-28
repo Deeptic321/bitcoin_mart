e1cc8be82e0445236fae359830eacc769663b989
Deeptic321 <77106368+Deeptic321@users.noreply.github.com>
2023-05-28 13:43:47 +0530

    Add files via upload

---
+++ bitcoin_mart/src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCl5hr3XT4C-bx08SJULWISQPLKy8OlOBM",
+    authDomain: "bitcoin-mart.firebaseapp.com",
+    projectId: "bitcoin-mart",
+    storageBucket: "bitcoin-mart.appspot.com",
+    messagingSenderId: "2578729339",
+    appId: "1:2578729339:web:ebffc719de9d938e4a55de"
+  };
+  export default firebaseConfig;
